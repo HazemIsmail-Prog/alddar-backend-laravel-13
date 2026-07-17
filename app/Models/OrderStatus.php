b9616afe6dec2038468a0fa90ebf0085171924f1
Hazem <hazem.ismail@hotmail.com>
2026-07-17 21:16:02 +0300

    assign constants for order statuses

--- app/Models/OrderStatus.php
+++ app/Models/OrderStatus.php
@@ -8,6 +8,14 @@ class OrderStatus extends Model
 {
     protected $guarded = [];
 
+    public const UNASSIGNED = 1;
+    public const HOLDED = 2;
+    public const ASSIGNED = 3;
+    public const RECEIVED = 4;
+    public const ARRIVED = 5;
+    public const COMPLETED = 6;
+    public const CANCELLED = 7;
+
     public function orders()
     {
         return $this->hasMany(Order::class);
